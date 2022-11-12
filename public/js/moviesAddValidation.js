b09e5adda4006ccd1d0ad0b2729b05ad33455ccb
CalebRR <yohannagraciano@gmail.com>
2022-11-12 09:07:40 -0300

    add validator release_date

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -10,12 +10,10 @@ window.onload = function () {
   //------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
   //-------------------DE REGISTRO DE PELÍCULAS------------------//
 
-  formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-  })
 
   const addMovies = $("addMovies");
   const elements = addMovies.elements;
+
 
   const msgError = (element, msg, event) => {
     $(element).style.color = "red";
@@ -24,20 +22,38 @@ window.onload = function () {
   };
 
   const cleanError = (element, { target }) => {
-    target.classList.remove("is-invalid","is-valid")
+    target.classList.remove("is-invalid", "is-valid");
     $(element).innerHTML = null;
   };
 
-  const validField = (element, {target}) => {
+  const validField = (element, { target }) => {
     $(element).innerHTML = null;
-    target.classList.remove('is-invalid')
-    target.classList.add('is-valid');
+    target.classList.remove("is-invalid");
+    target.classList.add("is-valid");
   };
+
+  const checkFields = () => {
+    let error = false;
+    for (let i = 0; i < elements.length - 1; i++) {
+      
+      if(!elements[i].value || elements[i].classList.contains('is-invalid')) {
+        error = true
+      }
+      console.log(error)
+    }
+  
+    if(!error){
+      $('btn-submit').disabled = false;
+    }else {
+      $('btn-submit').disabled = true;
+    }
+  }
 
   
 
+  /* validaci{on titulo} */
   $("title").addEventListener("focus", function (e) {
-    cleanError("titleMsg", e)
+    cleanError("titleMsg", e);
   });
   $("title").addEventListener("blur", function (e) {
     switch (true) {
@@ -45,16 +61,21 @@ window.onload = function () {
         msgError("titleMsg", "El titulo es obligatorio", e);
         break;
       case this.value.trim().length < 2:
-        msgError("titleMsg", "El titulo debe tener como minimo 2 caracteres", e);
+        msgError(
+          "titleMsg",
+          "El titulo debe tener como minimo 2 caracteres",
+          e
+        );
         break;
       default:
-        validField("titleMsg", e)
+        validField("titleMsg", e);
         break;
     }
+    checkFields();
   });
 
   $("rating").addEventListener("focus", function (e) {
-    cleanError("ratingMsg", e)
+    cleanError("ratingMsg", e);
   });
   $("rating").addEventListener("blur", function (e) {
     switch (true) {
@@ -65,9 +86,10 @@ window.onload = function () {
         msgError("ratingMsg", "Indicar valor entre 0 y 10", e);
         break;
       default:
-        validField("ratingMsg", e)
+        validField("ratingMsg", e);
         break;
     }
+    checkFields();
   });
 
   $("awards").addEventListener("blur", function (e) {
@@ -79,23 +101,26 @@ window.onload = function () {
         msgError("awardsMsg", "Indicar valor entre 0 y 10", e);
         break;
       default:
-        validField("awardsMsg", e)
+        validField("awardsMsg", e);
         break;
     }
+    checkFields();
   });
 
-  $("release_date").addEventListener("blur", function (e) {
+   $("release_date").addEventListener("blur", function (e) {
+    let fecha = /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/
     switch (true) {
-      case this.value !== /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/:
+      case !this.value === fecha
+       :
         msgError("release_dateMsg", "La Fecha es obligatoria", e);
         break;
       default:
-        validField("release_dateMsg", e)
+        validField("release_dateMsg", e);
         break;
     }
-  });
- 
- 
+    checkFields();
+  }); 
+
   $("length").addEventListener("blur", function (e) {
     switch (true) {
       case !this.value.trim():
@@ -105,24 +130,21 @@ window.onload = function () {
         msgError("lengthMsg", "Indicar duración entre 60 y 360 minutos", e);
         break;
       default:
-        validField("lengthMsg", e)
+        validField("lengthMsg", e);
         break;
     }
+    checkFields();
   });
 
-  $("genre_id").addEventListener('blur', function(e) {
+  $("genre_id").addEventListener("blur", function (e) {
     switch (true) {
       case !this.value:
-        msgError("genre_idMsg", "El género es obligatorio", e)
+        msgError("genre_idMsg", "El género es obligatorio", e);
         break;
       default:
-        validField("genre_idMsg", e)
+        validField("genre_idMsg", e);
         break;
     }
-  
-    checkFields()
-  
-  
+    checkFields();
   });
-
 };
